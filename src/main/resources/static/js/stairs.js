0c49a891da1acd04d348bcf8c42c2130305d456b
Sanzhar <sanzhar.aubakirov@innoforce.kz>
2018-08-08 12:49:36 +0600

    Add algo to index.html

--- src/main/resources/static/js/stairs.js
+++ src/main/resources/static/js/stairs.js
@@ -1,31 +1,34 @@
-
 var text = "<tr>\n" +
   "            <td>{ARRAY}</td>\n" +
   "            <td>{ANSWER}</td>\n" +
+  "            <td>{ALGORITHM}</td>\n" +
   "        </tr>";
 
-$( "#target" ).submit(function( event ) {
+$("#target").submit(function (event) {
   event.preventDefault();
 
+  var algo = $("#type").val();
   var term = $("#search").val();
+
   var re = new RegExp("^(\\d,?)*$");
   if (re.test(term)) {
-    var data = JSON.stringify($("#search").val().split(',').map(Number));
+    var data = JSON.stringify(term.split(',').map(Number));
     console.log(data);
 
     $.ajax({
-      url:"api/stairsArray/?type="+$("#type").val(),
-      type:"POST",
-      data:data,
-      contentType:"application/json; charset=utf-8",
-      dataType:"json",
-      success: function(data){
+      url: "api/stairsArray/?type=" + algo,
+      type: "POST",
+      data: data,
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function (data) {
         console.log(data);
 
 
         var card = String(text)
-          .replace(/\{ARRAY}/g, $("#search").val())
-          .replace(/\{ANSWER}/g, data);
+          .replace(/\{ARRAY}/g, term)
+          .replace(/\{ANSWER}/g, data)
+          .replace(/\{ALGORITHM}/g, algo);
         $('#container').prepend(card);
       },
     })
@@ -33,7 +36,8 @@ $( "#target" ).submit(function( event ) {
   } else {
     var card = String(text)
       .replace(/\{ARRAY}/g, $("#search").val())
-      .replace(/\{ANSWER}/g, "Wrong input, enter integers delimited by ','");
+      .replace(/\{ANSWER}/g, "Wrong input, enter integers delimited by ','")
+  .replace(/\{ALGORITHM}/g, "NONE");
     $('#container').prepend(card);
   }
 });
